b974ec4812acb85d6627e9b4aea84dc7b3b7bb5f
Guillaume Garcia <garcia62u@tncy-0-4-02.telecomnancy.site.univ-lorraine.fr>
2017-09-28 10:13:24 +0200

    Firtst commit with first source code

---
+++ testios-le-2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testios-le-2
+//
+//  Created by Guillaume Garcia on 28/09/2017.
+//  Copyright © 2017 Telecom Nancy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testios-le-2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testios-le-2
+//
+//  Created by Guillaume Garcia on 28/09/2017.
+//  Copyright © 2017 Telecom Nancy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
